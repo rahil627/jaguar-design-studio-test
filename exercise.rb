63097511a11fe073fabc133a7ff9c99f562556ae
rathewolf <1132053+rahil627@users.noreply.github.com>
2025-02-24 15:22:10 -0800

    string replace attempt

--- exercise.rb
+++ exercise.rb
@@ -1,10 +1,31 @@
-class Exercise
+# github.dev?
+#  - no extensions :(
+# zed?
+#  - chromeos linux contianer problem..?
+# ruby playground? lol
 
+class Exercise
+  
   # Assume that "str" is a sequence of words separated by spaces.
   # Return a string in which every word in "str" that exceeds 4 characters is replaced with "marklar".
   # If the word being replaced has a capital first letter, it should instead be replaced with "Marklar".
   def self.marklar(str)
-    # TODO: Implement this method
+    words = str.split(" ")
+    
+    words.each_with_index do |word, i| # vs map
+      if word.length > 4
+        if word[0] == word[0].upcase # no char type in ruby
+          words[i] = "Marklar"
+        else
+          words[i] = "marklar"
+        end
+      else
+          # words[i] = word
+      end
+    end
+
+    words # for clarity
+    
   end
 
   # Return the sum of all even numbers in the Fibonacci sequence, up to
